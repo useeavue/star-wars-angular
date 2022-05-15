5bdcb2ea1f5aa83acd65bba07345f0e21d89237d
Maksim Makarov <therealbrookebond@gmail.com>
2022-05-15 18:58:27 +0400

    added pagination

--- src/app/components/planets-list/planets-list.component.ts
+++ src/app/components/planets-list/planets-list.component.ts
@@ -11,6 +11,8 @@ import { Planet } from 'src/app/shared/types/IPlanet';
 })
 export class PlanetsListComponent implements OnInit, OnDestroy {
   public planetsList: Planet[] = [];
+  public nextPage: string | null = '';
+  public previousPage: string | null = '';
   private subscription: Subscription;
 
   constructor(private planetsDataService: PlanetsDataService) {
@@ -21,10 +23,13 @@ export class PlanetsListComponent implements OnInit, OnDestroy {
     this.subscription.add(this.loadPlanets(PLANETS_API_LINK));
   }
 
-  public loadPlanets(url: string) {
+  public loadPlanets(url: string | null) {
+    if (!url) return;
     this.subscription.add(
       this.planetsDataService.getDataByUrl(url).subscribe((res) => {
         this.planetsList = res.results;
+        this.nextPage = res.next;
+        this.previousPage = res.previous;
       })
     );
   }
